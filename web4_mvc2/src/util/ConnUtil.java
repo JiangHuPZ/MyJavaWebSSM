dc87b200ea8f9617710d7027587385b97c495763
Jayce <showertaker@163.com>
2022-05-10 14:46:43 -0400

    Use database connection pool

--- web4_mvc2/src/util/ConnUtil.java
+++ web4_mvc2/src/util/ConnUtil.java
@@ -1,28 +1,41 @@
 package util;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * @author Jayce Xu
  * @create 2022504
  */
 public class ConnUtil {
-    public static final String DRIVER = "com.mysql.jdbc.Driver" ;
-    public static final String URL = "jdbc:mysql://localhost:3306/fruitdb?useUnicode=true&characterEncoding=utf-8&useSSL=false";
-    public static final String USER = "root";
-    public static final String PWD = "abc123";
+//    public static final String DRIVER = "com.mysql.jdbc.Driver" ;
+//    public static final String URL = "jdbc:mysql://localhost:3306/fruitdb?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+//    public static final String USER = "root";
+//    public static final String PWD = "abc123";
 
     private static ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
 
     public static Connection getConn() {
         try {
+            InputStream is = ConnUtil.class.getClassLoader().getResourceAsStream("jdbc.properties");
+            Properties properties = new Properties();
+            properties.load(is);
+
+            DataSource dataSource = DruidDataSourceFactory.createDataSource(properties);
+
+            return dataSource.getConnection();
             //1.加载驱动
-            Class.forName(DRIVER);
+//            Class.forName(DRIVER);
             //2.通过驱动管理器获取连接对象
-            return DriverManager.getConnection(URL, USER, PWD);
-        } catch (ClassNotFoundException | SQLException e) {
+//            return DriverManager.getConnection(URL, USER, PWD);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null ;
