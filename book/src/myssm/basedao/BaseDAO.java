0de8c4742660f47af44753743b84a2bdeabd701f
JiangHuPZ <93489186+JiangHuPZ@users.noreply.github.com>
2022-05-19 17:33:43 -0400

    Update BaseDAO.java

--- book/src/myssm/basedao/BaseDAO.java
+++ book/src/myssm/basedao/BaseDAO.java
@@ -17,14 +17,11 @@ public abstract class BaseDAO<T> {
 
 
     public BaseDAO() {
-        //getClass() 获取Class对象，当前我们执行的是new FruitDAOImpl() , 创建的是FruitDAOImpl的实例
-        //那么子类构造方法内部首先会调用父类（BaseDAO）的无参构造方法
-        //因此此处的getClass()会被执行，但是getClass获取的是FruitDAOImpl的Class
-        //所以getGenericSuperclass()获取到的是BaseDAO的Class
+        
         Type genericType = getClass().getGenericSuperclass();
-        //ParameterizedType 参数化类型
+        
         Type[] actualTypeArguments = ((ParameterizedType) genericType).getActualTypeArguments();
-        //获取到的<T>中的T的真实的类型
+        
         Type actualType = actualTypeArguments[0];
 
         try {
@@ -44,7 +41,6 @@ public abstract class BaseDAO<T> {
 
     }
 
-    //给预处理命令对象设置参数
     private void setParams(PreparedStatement psmt , Object... params) throws SQLException {
         if(params!=null && params.length>0){
             for (int i = 0; i < params.length; i++) {
@@ -53,7 +49,6 @@ public abstract class BaseDAO<T> {
         }
     }
 
-    //执行更新，返回影响行数
     protected int executeUpdate(String sql , Object... params) {
         boolean insertFlag = false ;
         insertFlag = sql.trim().toUpperCase().startsWith("INSERT");
@@ -81,20 +76,20 @@ public abstract class BaseDAO<T> {
         }
     }
 
-    //通过反射技术给obj对象的property属性赋propertyValue值
+
     private void setValue(Object obj ,  String property , Object propertyValue) throws NoSuchFieldException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException {
         Class clazz = obj.getClass();
 
-        //获取property这个字符串对应的属性名 ， 比如 "fid"  去找 obj对象中的 fid 属性
+      
         Field field = clazz.getDeclaredField(property);
         if(field!=null){
 
-            //获取当前字段的类型名称
+
             String typeName = field.getType().getName();
-            //判断如果是自定义类型，则需要调用这个自定义类的带一个参数的构造方法，创建出这个自定义的实例对象，然后将实例对象赋值给这个属性
+
 
             if(isMyType(typeName)){
-                //假设typeName是"com.atguigu.qqzone.pojo.UserBasic"
+               
                 Class typeNameClass = Class.forName(typeName);
                 Constructor constructor = typeNameClass.getDeclaredConstructor(Integer.class);
                 propertyValue = constructor.newInstance(propertyValue);
@@ -116,7 +111,7 @@ public abstract class BaseDAO<T> {
         return !isNotMyType(typeName);
     }
 
-    //执行复杂查询，返回例如统计结果
+  
     protected Object[] executeComplexQuery(String sql , Object... params){
         conn = getConn() ;
         try{
@@ -124,14 +119,13 @@ public abstract class BaseDAO<T> {
             setParams(psmt,params);
             rs = psmt.executeQuery();
 
-            //通过rs可以获取结果集的元数据
-            //元数据：描述结果集数据的数据 , 简单讲，就是这个结果集有哪些列，什么类型等等
+           
 
             ResultSetMetaData rsmd = rs.getMetaData();
-            //获取结果集的列数
+          
             int columnCount = rsmd.getColumnCount();
             Object[] columnValueArr = new Object[columnCount];
-            //6.解析rs
+            
             if(rs.next()){
                 for(int i = 0 ; i<columnCount;i++){
                     Object columnValue = rs.getObject(i+1);     //33    苹果      5
@@ -147,7 +141,7 @@ public abstract class BaseDAO<T> {
         return null ;
     }
 
-    //执行查询，返回单个实体对象
+   
     protected T load(String sql , Object... params){
         conn = getConn() ;
         try{
@@ -155,13 +149,12 @@ public abstract class BaseDAO<T> {
             setParams(psmt,params);
             rs = psmt.executeQuery();
 
-            //通过rs可以获取结果集的元数据
-            //元数据：描述结果集数据的数据 , 简单讲，就是这个结果集有哪些列，什么类型等等
+         
 
             ResultSetMetaData rsmd = rs.getMetaData();
-            //获取结果集的列数
+           
             int columnCount = rsmd.getColumnCount();
-            //6.解析rs
+          
             if(rs.next()){
                 T entity = (T)entityClass.newInstance();
 
@@ -180,7 +173,7 @@ public abstract class BaseDAO<T> {
         return null ;
     }
 
-    //执行查询，返回List
+    
     protected List<T> executeQuery(String sql , Object... params){
         List<T> list = new ArrayList<>();
         conn = getConn() ;
@@ -189,19 +182,17 @@ public abstract class BaseDAO<T> {
             setParams(psmt,params);
             rs = psmt.executeQuery();
 
-            //通过rs可以获取结果集的元数据
-            //元数据：描述结果集数据的数据 , 简单讲，就是这个结果集有哪些列，什么类型等等
-
+           
             ResultSetMetaData rsmd = rs.getMetaData();
-            //获取结果集的列数
+            
             int columnCount = rsmd.getColumnCount();
-            //6.解析rs
+           
             while(rs.next()){
                 T entity = (T)entityClass.newInstance();
 
                 for(int i = 0 ; i<columnCount;i++){
-                    String columnName = rsmd.getColumnLabel(i+1);            //fid   fname   price
-                    Object columnValue = rs.getObject(i+1);     //33    苹果      5
+                    String columnName = rsmd.getColumnLabel(i+1);           
+                    Object columnValue = rs.getObject(i+1);   
                     setValue(entity,columnName,columnValue);
                 }
                 list.add(entity);
